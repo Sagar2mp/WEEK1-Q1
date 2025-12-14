d38c456c26c873494b8abb4686c891733fe65435
Sagar2mp <24331a05j0@mvgrce.edu.in>
2025-12-14 21:33:04 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,8 @@
+n1=10
+char='s'
+n2=2.4
+strl1="sagar"
+print("Data type int n1: ",n1,"\t type: ",type(n1))
+print("Data type char : ",char,"\t type: ",type(char))
+print("Data type float n2: ",n2,"\t type: ",type(n2))
+print("Data type string : ",strl1,"\t type: ",type(strl1))
